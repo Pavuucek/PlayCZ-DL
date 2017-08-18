e550ae0c4da5063cd32580342ceeee9f2868207c
Michal Kuncl <michal.kuncl@gmail.com>
2017-08-18 11:54:05 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayCZ2TvHeadend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
