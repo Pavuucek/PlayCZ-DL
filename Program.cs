2bddde0f4e8b36476a65343b38defc6a674db0a7
Michal Kuncl <michal.kuncl@gmail.com>
2017-12-29 15:55:26 +0100

    optimize some code and use current directory instead of assembly directory

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -17,12 +16,13 @@ namespace PlayCZ2TvHeadend
 
         private static void Main(string[] args)
         {
+            Console.WriteLine("PlayCZ2TvHeadend Starting...");
+            Console.WriteLine();
             GetRadios();
             GenM3U(false);
             GenM3U(true);
-            Console.WriteLine("");
-            Console.WriteLine("end");
-            //Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Finished!");
         }
 
         private static void GetRadios()
@@ -36,13 +36,14 @@ namespace PlayCZ2TvHeadend
 
             foreach (Match match in r.Matches(radios))
             {
-                var radio = new Radio();
-                radio.Title = match.Groups["1"].Value;
-                radio.Description = match.Groups["2"].Value;
-                radio.Shortcut = match.Groups["3"].Value;
-                //radio.StreamList = GetStreams(match.Groups["3"].Value).OrderBy(s => s).ToList();
-                radio.StreamList = new List<string>();
-                radio.LogoUrl = $"http://api.play.cz/static/radio_logo/t200/{match.Groups["3"].Value}.png";
+                var radio = new Radio
+                {
+                    Title = match.Groups["1"].Value,
+                    Description = match.Groups["2"].Value,
+                    Shortcut = match.Groups["3"].Value,
+                    StreamList = new List<string>(),
+                    LogoUrl = $"http://api.play.cz/static/radio_logo/t200/{match.Groups["3"].Value}.png"
+                };
                 _radios.Add(radio);
             }
 
@@ -75,7 +76,7 @@ namespace PlayCZ2TvHeadend
             var reg = new Regex("<streams>(.+?)</streams>");
             foreach (Match match in reg.Matches(httpdata))
             {
-                var reg2 = new Regex(@"<(.+?)>(.+?)</loop></"); //</");
+                var reg2 = new Regex(@"<(.+?)>(.+?)</loop></");
                 foreach (Match match2 in reg2.Matches(match.Groups["1"].Value))
                 {
                     var suffix = match2.Groups["1"].Value;
@@ -101,9 +102,9 @@ namespace PlayCZ2TvHeadend
                             httpdata2 = httpdata2.Replace("\n", string.Empty);
                             httpdata2 = httpdata2.Replace("\t", string.Empty);
                             var reg4 = new Regex(@"<pubpoint>.+?CDATA\[(.+?)\]\]></pubpoint>");
-                            res = string.Empty;
-                            foreach (Match match4 in reg4.Matches(httpdata2))
-                                res += match4.Groups["1"].Value;
+                            var sb = new StringBuilder();
+                            foreach (Match match4 in reg4.Matches(httpdata2)) sb.Append(match4.Groups["1"].Value);
+                            res = sb.ToString();
                         }
 
                         result.AddRange(res.Split('\n'));
@@ -119,36 +120,38 @@ namespace PlayCZ2TvHeadend
             Console.Write("Generating ");
             if (tvHeadend) Console.Write("TvHeadend ");
             Console.WriteLine("PlayList...");
-            var result = "#EXTM3U\n";
+            var result = new StringBuilder();
+            result.AppendLine("#EXTM3U");
             foreach (var radio in _radios)
                 if (radio.StreamList.Count > 0)
                 {
-                    result += $"#EXTINF:-1 group-title=\"Play.cz\" radio=true tvg-logo={radio.LogoUrl},{radio.Title}\n";
+                    result.AppendLine(
+                        $"#EXTINF:-1 group-title=\"Play.cz\" radio=true tvg-logo={radio.LogoUrl},{radio.Title}");
                     var stream = radio.StreamList[0];
                     if (tvHeadend)
                     {
                         var r =
-                            $"pipe://ffmpeg -loglevel fatal -i {radio.StreamList[0]} -vn -acodec copy -flags +global_header -strict -2 -metadata service_provider={radio.StreamList[0]} -metadata service_name={radio.Shortcut} -f mpegts -mpegts_service_type digital_radio pipe:1\n";
+                            $"pipe://ffmpeg -loglevel fatal -i {radio.StreamList[0]} -vn -acodec copy -flags +global_header -strict -2 -metadata service_provider={radio.StreamList[0]} -metadata service_name={radio.Shortcut} -f mpegts -mpegts_service_type digital_radio pipe:1";
                         if (stream.ToLowerInvariant().Contains("mms:"))
                             r = r.Replace("-acodec copy", "-acodec aac"); // prekodovat wma stream
-                        result += r;
+                        result.AppendLine(r);
                     }
                     else
                     {
-                        result += radio.StreamList[0] + "\n";
+                        result.AppendLine(radio.StreamList[0]);
                     }
                 }
 
-            var currentDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\";
+            var currentDir = Directory.GetCurrentDirectory() + "\\";
             if (tvHeadend)
             {
                 Console.WriteLine(currentDir + "tvheadend.m3u");
-                File.WriteAllText(currentDir + "tvheadend.m3u", result);
+                File.WriteAllText(currentDir + "tvheadend.m3u", result.ToString());
             }
             else
             {
                 Console.WriteLine(currentDir + "playlist.m3u");
-                File.WriteAllText(currentDir + "playlist.m3u", result);
+                File.WriteAllText(currentDir + "playlist.m3u", result.ToString());
             }
         }
 
@@ -166,7 +169,6 @@ namespace PlayCZ2TvHeadend
         public static string ToWindows1250(this string source)
         {
             var windows1250 = Encoding.ASCII;
-            //Encoding.GetEncoding("windows-1250");
             var utf8 = Encoding.UTF8;
             var utfBytes = utf8.GetBytes(source);
             var win1250Bytes = Encoding.Convert(utf8, windows1250, utfBytes);
